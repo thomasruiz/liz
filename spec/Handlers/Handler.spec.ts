36bf9e9c2f07ccedcfc0c86ec1bee999cba2cbd2
Thomas Ruiz <thomas.ruiz@neoxia.com>
2016-09-16 17:28:21 +0200

    Err, fix test. It's friday night.

--- spec/Handlers/Handler.spec.ts
+++ spec/Handlers/Handler.spec.ts
@@ -1,21 +1,23 @@
 import * as chai from 'chai'
-import * as sinon from 'sinon'
 import * as sinonChai from 'sinon-chai'
 import { Bot } from '../../src/Bot'
 import { Message } from '../../src/Message'
-import SinonSpy = Sinon.SinonSpy
 import { Handler } from '../../src/Handlers/Handler'
+import SinonSpy = Sinon.SinonSpy
 
 chai.use(sinonChai)
 const expect = chai.expect
 
 describe('Handler', () => {
-    it('should throttle users correctly', () => {
+    it('should throttle users correctly', (done) => {
         const handler: StubHandler = new StubHandler(<Bot> <any> {})
         expect(handler.throttled('user')).to.equal(false)
         handler.throttle('user', .03)
         expect(handler.throttled('user')).to.equal(true)
-        setTimeout(() => expect(handler.throttled('user')).to.equal(false), 30)
+        setTimeout(() => {
+            expect(handler.throttled('user')).to.equal(false)
+            done()
+        }, 30)
     })
 })
 
